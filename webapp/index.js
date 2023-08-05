4edb693c699a47934bd4f5a206734f7b0a31f662
Anna Hartmann <anna.ig.hartmann@gmail.com>
2023-08-05 19:01:38 +0200

    setup index.html and index.js

---
+++ webapp/index.js
@@ -0,0 +1,17 @@
+sap.ui.define(
+  ["sap/m/Text", "sap/m/Button", "sap/m/MessageToast"],
+  function (Text, Button, MessageToast) {
+    "use strict";
+
+    new Text({
+      text: "Hello UI5!",
+    }).placeAt("content");
+
+    new Button({
+      text: "Ready...",
+      press: function () {
+        MessageToast.show("Taaadaaa");
+      },
+    }).placeAt("content");
+  }
+);
