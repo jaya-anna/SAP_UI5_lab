746189d8cc15da33e87397d7f657d6c5759eb178
Anna Hartmann <anna.ig.hartmann@gmail.com>
2023-08-05 21:47:24 +0200

    i18n.properties  added

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,24 +2,36 @@ sap.ui.define(
   [
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
-    "use strict"
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "use strict";
 
     return Controller.extend("quickstart.App", {
       onInit: function () {
         var oData = {
           recipient: {
-            name: "UI5"
-          }
+            name: "",
+          },
         };
         var oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+
+        var i18nModel = new ResourceModel({
+          bundleName: "quickstart.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
 
       onShowHello: function () {
-        MessageToast.show("Hello there");
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+        MessageToast.show(sMsg);
       },
     });
   }
